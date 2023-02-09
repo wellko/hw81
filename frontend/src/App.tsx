0c428223ee01b1c2b87fbbdc6395faf16a909242
Kwon Valerii <wellko24@gmail.com>
2023-02-10 00:47:04 +0600

    added form component to main app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Form from "./components/Form/Form";
+import {Container} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
+        <Container>
+            <h1>Shorten your link!</h1>
+            <Form/>
+        </Container>
+
     </div>
   );
 }
